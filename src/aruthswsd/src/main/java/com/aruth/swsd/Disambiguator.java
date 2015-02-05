6029cc82d76bba702d20bbd6c7c87127984f2b13
Janindu <gjanindu@gjanindu-Aspire-4740.(none)>
2015-02-05 22:56:31 +0530

    WrdNet added. Libraries added. WordNet Reader implemented

---
+++ src/aruthswsd/src/main/java/com/aruth/swsd/Disambiguator.java
@@ -0,0 +1,5 @@
+package com.aruth.swsd;
+
+public class Disambiguator {
+
+}
---
+++ src/aruthswsd/src/main/java/com/aruth/swsd/data/WordNetReader.java
@@ -0,0 +1,51 @@
+package com.aruth.swsd.data;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import net.sf.extjwnl.JWNLException;
+import net.sf.extjwnl.data.IndexWord;
+import net.sf.extjwnl.data.POS;
+import net.sf.extjwnl.dictionary.Dictionary;
+
+public class WordNetReader {
+
+	private final static String WORDNETPATH = "data/clean-file";
+
+	/**
+	 * @param String noun : the noun to be retrieved from the Sinhala WordNet. unicode character encoding
+	 * @return IndexWord word
+	 */
+	public static IndexWord getNounAsIndexWord (String noun) {
+		Dictionary dictionary = getDictionary();
+		IndexWord word = null;
+		
+		try {
+			word = dictionary.getIndexWord(POS.NOUN, noun);	
+		} catch (JWNLException e) {
+			System.out.println(e.getLocalizedMessage());
+		}
+		
+		return word;
+	}
+	
+	/**
+	 * 
+	 * @return Dictionary
+	 */
+	private static Dictionary getDictionary() {
+		FileInputStream inputStream;
+		Dictionary dictionary = null;
+		
+		try {
+			inputStream = new FileInputStream(WORDNETPATH);
+			dictionary = Dictionary.getInstance(inputStream);
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getLocalizedMessage());
+		} catch (JWNLException e) {
+			System.out.println(e.getLocalizedMessage());
+		}
+		
+		return dictionary;
+	}
+}
